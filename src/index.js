41c8b411f0f8b6312233127afbd4639686708b7d
LeonardoOstrowski <143043122+LeonardoOstrowski@users.noreply.github.com>
2024-10-30 18:10:01 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.scss';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/landing";
+import AdminIndex from "./pages/admin/AdminIndex";
+import LoginIndex from "./pages/login/LoginIndex";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,6 +13,8 @@ root.render(
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home />} />
+                <Route path='/login' element={<LoginIndex />} />
+                <Route path='/admin' element={<AdminIndex />} />
             </Routes>
         </BrowserRouter>
     </React.StrictMode>
--- src/pages/landing/components/Footer.js
+++ src/pages/landing/components/Footer.js
@@ -3,33 +3,43 @@ import React from 'react';
 function Footer() {
     return (
         <footer className="footer">
-            <div className="container">
-                <div className="footer-content">
-                    <div className="footer-section">
-                        <h3>Endereço</h3>
-                        <p>Ponto de acesso perto da estação de metrô</p>
-                        <p>Rua Industrial, CEP: 98765-444</p>
-                    </div>
-                    <div className="footer-section">
-                        <h3>Contato</h3>
-                        <p>Email: contato@exemplo.com</p>
-                        <p>Telefone: (11) 98765-4321</p>
-                    </div>
-                    <div className="footer-section">
-                        <h3>Redes Sociais</h3>
-                        <p>
-                            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer"
-                               className="button">Facebook</a>
-                            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer"
-                               className="button">Twitter</a>
-                            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"
-                               className="button">LinkedIn</a>
-                        </p>
+            <div className="footer-content">
+                <div className="footer-section location">
+                    <img src="https://imgur.com/ytKEGf8.png" alt="Logo" className="logo"/>
+                    <p>Ponto de acesso perto da estação autódromo</p>
+                    <p>ㅤ</p>
+                    <p>Endereço: Rua fulandital<br/>CEP: 98765-444</p>
+                </div>
+
+                <div className="footer-section subscribe">
+                    <label htmlFor="email">Seu Email</label>
+                    <div className="subscribe-container">
+                        <input type="email" id="email" placeholder="example@email.com"/>
+                        <button type="submit">Subscribe</button>
                     </div>
                 </div>
-                <div className="footer-bottom">
-                    <p>&copy; 2024 João Paulo Pedreiro. Todos os direitos reservados.</p>
+
+                <div className="footer-section links">
+                    <h3>Links</h3>
+                    <ul>
+                        <li><a href="#">Contact us</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Careers</a></li>
+                    </ul>
                 </div>
+
+                <div className="footer-section social">
+                    <h3>Redes sociais</h3>
+                    <ul>
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">LinkedIn</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="footer-bottom">
+                <p>copyright © 2024</p>
             </div>
         </footer>
     );
